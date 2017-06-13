090c5462314f8882300fdb4d236f993a4963ede6
wangyunzhong <wangyunzhong@rongcapital.cn>
2017-06-13 15:10:14 +0800

    stat once

--- src/main/java/org/yunzhong/service/biz/HistoryDataService.java
+++ src/main/java/org/yunzhong/service/biz/HistoryDataService.java
@@ -10,33 +10,41 @@ import org.yunzhong.service.model.HistoryDataStat;
 
 public interface HistoryDataService {
 
-	/**
-	 * @param datas
-	 * @return
-	 */
-	int batchInsert(List<HistoryData> datas);
+    /**
+     * @param datas
+     * @return
+     */
+    int batchInsert(List<HistoryData> datas);
 
-	/**
-	 * 连涨统计
-	 * 
-	 * @param dataId
-	 * @return
-	 */
-	Map<Integer, HistoryDataStat> stat(String dataId);
+    /**
+     * 连涨统计
+     * 
+     * @param dataId
+     * @return
+     */
+    Map<Integer, HistoryDataStat> stat(String dataId);
 
-	/**
-	 * 涨停统计
-	 * 
-	 * @param dataId
-	 * @return
-	 */
-	Map<Integer, HistoryDataStat> statUpStaying(String dataId, Double percentage);
+    /**
+     * 涨停统计
+     * 
+     * @param dataId
+     * @return
+     */
+    Map<Integer, HistoryDataStat> statUpStaying(String dataId, Double percentage);
 
-	/**
-	 * 连续涨之后，下一天的涨跌统计
-	 * 
-	 * @param param
-	 * @return
-	 */
-	HistoryDataPreStat preUpRate(StatParam param);
+    /**
+     * 连续涨之后，下一天的涨跌统计
+     * 
+     * @param param
+     * @return
+     */
+    HistoryDataPreStat preUpRate(StatParam param);
+
+    /**
+     * 连续涨之后，下一天的涨跌统计. 如果连续涨次数超过count，则本次连续涨只计算一次。
+     * 
+     * @param param
+     * @return
+     */
+    public HistoryDataPreStat preUpRateOnce(StatParam param);
 }
--- src/main/java/org/yunzhong/service/biz/impl/HistoryDataServiceImpl.java
+++ src/main/java/org/yunzhong/service/biz/impl/HistoryDataServiceImpl.java
@@ -29,157 +29,203 @@ import org.yunzhong.util.SockUtil;
 @Service
 @Transactional
 public class HistoryDataServiceImpl implements HistoryDataService {
-	private static final Logger log = LoggerFactory.getLogger(HistoryDataServiceImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(HistoryDataServiceImpl.class);
 
-	@Autowired
-	private HistoryDataDAO historyDao;
+    @Autowired
+    private HistoryDataDAO historyDao;
 
-	@Override
-	public int batchInsert(List<HistoryData> datas) {
-		log.debug("batch insert history info");
-		return historyDao.batchInsert(datas);
-	}
+    @Override
+    public int batchInsert(List<HistoryData> datas) {
+        log.debug("batch insert history info");
+        return historyDao.batchInsert(datas);
+    }
 
-	@Override
-	public Map<Integer, HistoryDataStat> stat(String dataId) {
-		Map<Integer, HistoryDataStat> result = new HashMap<>();
-		List<HistoryData> datas = historyDao.selectById(dataId);
-		if (!CollectionUtils.isEmpty(datas)) {
-			List<HistoryData> cacheData = new ArrayList<>();
-			Integer cacheCount = 0;
-			HistoryData old = null;
-			Date startDate = null;
-			Date endDate = null;
-			for (HistoryData data : datas) {
-				if (startDate == null) {
-					startDate = data.getDate();
-				}
-				if (old == null) {
-					old = data;
-					continue;
-				}
-				if (old.getClose() < data.getClose()) {// 涨
-					cacheCount++;
-					cacheData.add(data);
-					old = data;
-					endDate = data.getDate();
-				} else {// 未涨
-					old = data;
-					if (cacheCount <= 1) { // 没有连涨
-					} else { // 有连涨，记录历史
-						HistoryDataStat resultC = result.get(cacheCount);
-						if (resultC == null) {
-							resultC = new HistoryDataStat();
-							result.put(cacheCount, resultC);
-						}
-						HistoryDataCollection collection = new HistoryDataCollection();
-						collection.setDatas(cacheData);
-						collection.setStart(startDate);
-						collection.setEnd(endDate);
-						resultC.getDataCollection().add(collection);
-					}
-					cacheCount = 0;
-					cacheData = new ArrayList<>();
-					startDate = null;
-				}
-			}
-		}
-		return result;
-	}
+    @Override
+    public Map<Integer, HistoryDataStat> stat(String dataId) {
+        Map<Integer, HistoryDataStat> result = new HashMap<>();
+        List<HistoryData> datas = historyDao.selectById(dataId);
+        if (!CollectionUtils.isEmpty(datas)) {
+            List<HistoryData> cacheData = new ArrayList<>();
+            Integer cacheCount = 0;
+            HistoryData old = null;
+            Date startDate = null;
+            Date endDate = null;
+            for (HistoryData data : datas) {
+                if (startDate == null) {
+                    startDate = data.getDate();
+                }
+                if (old == null) {
+                    old = data;
+                    continue;
+                }
+                if (old.getClose() < data.getClose()) {// 涨
+                    cacheCount++;
+                    cacheData.add(data);
+                    old = data;
+                    endDate = data.getDate();
+                } else {// 未涨
+                    old = data;
+                    if (cacheCount <= 1) { // 没有连涨
+                    } else { // 有连涨，记录历史
+                        HistoryDataStat resultC = result.get(cacheCount);
+                        if (resultC == null) {
+                            resultC = new HistoryDataStat();
+                            result.put(cacheCount, resultC);
+                        }
+                        HistoryDataCollection collection = new HistoryDataCollection();
+                        collection.setDatas(cacheData);
+                        collection.setStart(startDate);
+                        collection.setEnd(endDate);
+                        resultC.getDataCollection().add(collection);
+                    }
+                    cacheCount = 0;
+                    cacheData = new ArrayList<>();
+                    startDate = null;
+                }
+            }
+        }
+        return result;
+    }
 
-	@Override
-	public Map<Integer, HistoryDataStat> statUpStaying(String dataId, Double percentage) {
-		Map<Integer, HistoryDataStat> result = new HashMap<>();
-		List<HistoryData> datas = historyDao.selectById(dataId);
-		if (!CollectionUtils.isEmpty(datas)) {
-			List<HistoryData> cacheData = new ArrayList<>();
-			Integer cacheCount = 0;
-			HistoryData old = null;
-			Date startDate = null;
-			Date endDate = null;
-			for (HistoryData data : datas) {
-				if (startDate == null) {
-					startDate = data.getDate();
-				}
-				if (old == null) {
-					old = data;
-					continue;
-				}
-				if (SockUtil.upStaying(old.getClose(), data.getClose(), percentage)) {// 涨停
-					cacheCount++;
-					cacheData.add(data);
-					old = data;
-					endDate = data.getDate();
-				} else {// 未涨停
-					old = data;
-					if (cacheCount <= 1) { // 没有连涨
-					} else { // 有连涨，记录历史
-						HistoryDataStat resultC = result.get(cacheCount);
-						if (resultC == null) {
-							resultC = new HistoryDataStat();
-							resultC.setCount(cacheCount);
-							result.put(cacheCount, resultC);
-						}
-						HistoryDataCollection collection = new HistoryDataCollection();
+    @Override
+    public Map<Integer, HistoryDataStat> statUpStaying(String dataId, Double percentage) {
+        Map<Integer, HistoryDataStat> result = new HashMap<>();
+        List<HistoryData> datas = historyDao.selectById(dataId);
+        if (!CollectionUtils.isEmpty(datas)) {
+            List<HistoryData> cacheData = new ArrayList<>();
+            Integer cacheCount = 0;
+            HistoryData old = null;
+            Date startDate = null;
+            Date endDate = null;
+            for (HistoryData data : datas) {
+                if (startDate == null) {
+                    startDate = data.getDate();
+                }
+                if (old == null) {
+                    old = data;
+                    continue;
+                }
+                if (SockUtil.upStaying(old.getClose(), data.getClose(), percentage)) {// 涨停
+                    cacheCount++;
+                    cacheData.add(data);
+                    old = data;
+                    endDate = data.getDate();
+                } else {// 未涨停
+                    old = data;
+                    if (cacheCount <= 1) { // 没有连涨
+                    } else { // 有连涨，记录历史
+                        HistoryDataStat resultC = result.get(cacheCount);
+                        if (resultC == null) {
+                            resultC = new HistoryDataStat();
+                            resultC.setCount(cacheCount);
+                            result.put(cacheCount, resultC);
+                        }
+                        HistoryDataCollection collection = new HistoryDataCollection();
 
-						collection.setDatas(cacheData);
-						collection.setStart(startDate);
-						collection.setEnd(endDate);
-						resultC.getDataCollection().add(collection);
-						resultC.setUpCount(resultC.getDataCollection().size());
-					}
-					cacheCount = 0;
-					cacheData = new ArrayList<>();
-					startDate = null;
-				}
-			}
-		}
-		return result;
-	}
+                        collection.setDatas(cacheData);
+                        collection.setStart(startDate);
+                        collection.setEnd(endDate);
+                        resultC.getDataCollection().add(collection);
+                        resultC.setUpCount(resultC.getDataCollection().size());
+                    }
+                    cacheCount = 0;
+                    cacheData = new ArrayList<>();
+                    startDate = null;
+                }
+            }
+        }
+        return result;
+    }
 
-	@Override
-	public HistoryDataPreStat preUpRate(StatParam param) {
-		List<HistoryData> datas = historyDao.selectById(param.getDataId());
-		HistoryDataPreStat result = new HistoryDataPreStat();
-		result.setDataId(param.getDataId());
-		result.setPercentage(param.getPercentage());
-		result.setUpStayCount(param.getUpStayCount());
+    @Override
+    public HistoryDataPreStat preUpRateOnce(StatParam param) {
+        List<HistoryData> datas = historyDao.selectById(param.getDataId());
+        HistoryDataPreStat result = new HistoryDataPreStat();
+        result.setDataId(param.getDataId());
+        result.setPercentage(param.getPercentage());
+        result.setUpStayCount(param.getUpStayCount());
 
-		if (CollectionUtils.isEmpty(datas)) {
-			return result;
-		}
+        if (CollectionUtils.isEmpty(datas)) {
+            return result;
+        }
 
-		int count = 0;
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-		for (HistoryData data : datas) {
-			if (data.getUpRate() == null) {
-				continue;
-			}
-			if (count == param.getUpStayCount()) { // 连涨次数刚好，看当前的data是跌还是涨。
-				log.info("date: " + formatter.format(data.getDate()) + " | uprate: " + data.getUpRate());
-				result.totalCountPlus();
-				if (data.getUpRate() > 0) {
-					result.upCountPlus();
-				} else if (data.getUpRate() < 0) {
-					result.downCountPlus();
-				} else {
-					result.unchangeCountPlus();
-				}
-				if (data.getUpRate() >= param.getPercentage()) {
-					count++;
-				} else {
-					count = 0;
-				}
-			} else {// 如果连涨的次数少于目标，则继续累计；如果连涨的次数多余目标，继续向下查看，直到结束本次连涨。
-				if (data.getUpRate() >= param.getPercentage()) {
-					count++;
-				} else {
-					count = 0;
-				}
-			}
+        int count = 0;
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        for (HistoryData data : datas) {
+            if (data.getUpRate() == null) {
+                continue;
+            }
+            if (count == param.getUpStayCount()) { // 连涨次数刚好，看当前的data是跌还是涨。
+                log.info("date: " + formatter.format(data.getDate()) + " | uprate: " + data.getUpRate());
+                result.totalCountPlus();
+                if (data.getUpRate() > 0) {
+                    result.upCountPlus();
+                } else if (data.getUpRate() < 0) {
+                    result.downCountPlus();
+                } else {
+                    result.unchangeCountPlus();
+                }
+                if (data.getUpRate() >= param.getPercentage()) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+            } else {// 如果连涨的次数少于目标，则继续累计；如果连涨的次数多余目标，继续向下查看，直到结束本次连涨。
+                if (data.getUpRate() >= param.getPercentage()) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+            }
 
-		}
-		return result;
-	}
+        }
+        return result;
+    }
+
+    @Override
+    public HistoryDataPreStat preUpRate(StatParam param) {
+        List<HistoryData> datas = historyDao.selectById(param.getDataId());
+        HistoryDataPreStat result = new HistoryDataPreStat();
+        result.setDataId(param.getDataId());
+        result.setPercentage(param.getPercentage());
+        result.setUpStayCount(param.getUpStayCount());
+
+        if (CollectionUtils.isEmpty(datas)) {
+            return result;
+        }
+
+        int count = 0;
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        for (HistoryData data : datas) {
+            if (data.getUpRate() == null) {
+                continue;
+            }
+            if (count >= param.getUpStayCount()) { // 连涨次数刚好，看当前的data是跌还是涨。
+                log.info("date: " + formatter.format(data.getDate()) + " | uprate: " + data.getUpRate());
+                result.totalCountPlus();
+                if (data.getUpRate() > 0) {
+                    result.upCountPlus();
+                } else if (data.getUpRate() < 0) {
+                    result.downCountPlus();
+                } else {
+                    result.unchangeCountPlus();
+                }
+                if (data.getUpRate() >= param.getPercentage()) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+            } // 如果连涨的次数少于目标，则继续累计；如果连涨的次数多余目标，继续向下查看，直到结束本次连涨。
+            else {
+                if (data.getUpRate() >= param.getPercentage()) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+            }
+
+        }
+        return result;
+    }
 
 }
